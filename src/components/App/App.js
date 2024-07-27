0338931ccf579da3b3d32457902abc921b938d56
olijwood <olijwood@gmail.com>
2024-07-27 14:12:07 +0100

    Added Spotify Util

---
+++ src/components/App/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useCallback } from "react";
+import './App.css';
+
+import Playlist from "../Playlist/Playlist";
+import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Ja<span className="highlight">mmm</span>ing
+      </h1>
+      <div className="App">
+        <SearchBar />
+        <div className="App-playlist">
+          <SearchResults />
+          <Playlist
+            // playlistName={playlistName}
+            // playlistTracks={playlistTracks}
+            // onNameChange={updatePlaylistName}
+            // onRemove={removeTrack}
+            // onSave={savePlaylist}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
